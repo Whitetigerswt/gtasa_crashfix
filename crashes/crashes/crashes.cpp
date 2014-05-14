f82a9c68ebe6ff69848d6c30d93065c200a81607
Whitetiger <Whitetigerswt@users.noreply.github.com>
2014-05-14 19:59:07 -0400

    update crashes.cpp
    - update crashes.cpp to add the changes of MTA:SA blue r6415

--- crashes/crashes/crashes.cpp
+++ crashes/crashes/crashes.cpp
@@ -627,6 +627,9 @@ void _declspec(naked) HOOK_CrashFix_Misc21 ()
         cmp     ecx, 0x480
         jb      cont  // Skip much code if ecx is low
 
+	cmp     ecx, 0xfffffff0
+        ja      cont  // Skip much code if ecx is high
+
         // continue standard path
         mov     edx, [ecx+590h]
         jmp     RETURN_CrashFix_Misc21
